a9d15b8ad3050eccc4ccd32af5dffe7c577c6e44
PC <PC@192.168.31.154>
2018-03-25 17:08:43 +0300

    removed StringBuilder

--- FlowersProject/src/Bouquet.java
+++ FlowersProject/src/Bouquet.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 	public interface Bouquet {
 
 		public double getFinalPrice(ArrayList<Flowers> list, double sum);
-		public StringBuilder findFlowerByLength(double a, double b, ArrayList<Flowers> list);
 		public void displayCollection(ArrayList<Flowers> list);
 		public ArrayList<Flowers> createFirstCollection();
 }
--- FlowersProject/src/Main.java
+++ FlowersProject/src/Main.java
@@ -25,13 +25,13 @@ public class Main implements Bouquet {
 	// create collection
 	public ArrayList<Flowers> createFirstCollection(){
 		ArrayList<Flowers> list = new ArrayList<Flowers>();
-		Roses rosesOne = new Roses("Red Rose", 25, 10, 1);
+		Roses rosesOne = new Roses("Pink Rose", 25, 10, 1);
 		list.add(rosesOne);
 		Roses rosesTwo = new Roses("White Rose", 30, 15, 2);
 		list.add(rosesTwo);
 		Tulips tulipsOne = new Tulips("Avory Tulip", 7.5, 10, 1);
 		list.add(tulipsOne);
-		Tulips tulipsTwo = new Tulips("Pink Tulip", 8, 12, 2);
+		Tulips tulipsTwo = new Tulips("Red Tulip", 8, 12, 2);
 		list.add(tulipsTwo);
 		Orchids orchidOne = new Orchids("Violet Orchid", 150, 25, 3);
 		list.add(orchidOne);
@@ -43,7 +43,7 @@ public class Main implements Bouquet {
 	// display collection
 	public void displayCollection(ArrayList<Flowers> list) {
 		int i = 1;
-		System.out.println("ID: " + " NAME:    " + " 	    Price: " + "	Length:  " + " Fresh:");
+		System.out.println("ID: " + " NAME:    " + " 	    	  Price: " + "	Length:  " + " Fresh:");
 		for (Flowers vault : list) {
 			
 				System.out.println(i + ".   " + vault.getName() + "  	  " + vault.getPrice() + "		" + vault.getLength()
@@ -66,19 +66,13 @@ public class Main implements Bouquet {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		sc.close();
-		StringBuilder item = findFlowerByLength(a, b, list);
-		System.out.println("\nYour flowers are: " + item);
-		}
-
-		public StringBuilder findFlowerByLength(double a, double b, ArrayList<Flowers> list) {
-		StringBuilder flower = new StringBuilder();
+	
+		int j = 1;
 		for (Flowers vault : list) {
-			if ((vault.getLength() >= a && vault.getLength() <= b) || (vault.getLength() >= b && vault.getLength() <= a)) {
-				flower.append(vault.getName()).append("(Length= ").append(vault.getLength()).append(") ");
-			}
+			if ((vault.getLength() >= a && vault.getLength() <= b) || (vault.getLength() >= b && vault.getLength() <= a)){
+				System.out.println("\nYour flower is: " + vault.getName() + ", length=" + vault.getLength());
+				}
+			j++;
 		}
-		
-				return flower;
-			
-		}	
-}
+	}
+}	
